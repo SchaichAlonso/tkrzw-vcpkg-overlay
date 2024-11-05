663fccddbb0e39de43c5488f62bcd57d85306816
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-11-06 00:48:24 +0900

    Add tools option to vcpkg

--- tkrzw/portfile.cmake
+++ tkrzw/portfile.cmake
@@ -4,7 +4,11 @@ vcpkg_from_github(
     SHA512 0d87fc3d3e1d52229954a0b5588bd0cfd5bb53ad820fd829d8c72c94735fbb678e6f5947c6404ad214d8ad42cf6ed8db97ae7fd55f8ca13a8ebb5611d1e07640
     REF 3653a70a3a8d02d39cb2e087d057e4159ad86f2a
 )
-
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools           WITH_TOOLS
+)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
